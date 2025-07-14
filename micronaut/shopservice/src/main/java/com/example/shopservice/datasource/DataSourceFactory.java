43afa0e58544ed57eab6ca1a9cf45621d95276e3
rogerio <rrobetti@gmail.com>
2025-07-14 20:30:42 +0100

    Shopservice micronaut modified to use OJP. Workflow updated build ojp-jdbc latest before running tests of micronaut.

--- micronaut/shopservice/src/main/java/com/example/shopservice/datasource/DataSourceFactory.java
+++ micronaut/shopservice/src/main/java/com/example/shopservice/datasource/DataSourceFactory.java
@@ -4,9 +4,11 @@ import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Value;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
-import org.h2.jdbcx.JdbcDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Factory
 public class DataSourceFactory {
@@ -15,12 +17,39 @@ public class DataSourceFactory {
     public DataSource dataSource(
             @Value("${datasources.default.url}") String url,
             @Value("${datasources.default.username}") String user,
-            @Value("${datasources.default.password}") String password
-    ) {
-        JdbcDataSource ds = new JdbcDataSource();
-        ds.setURL(url);
-        ds.setUser(user);
-        ds.setPassword(password);
+            @Value("${datasources.default.password}") String password,
+            @Value("${datasources.default.driver-class-name}") String driver
+    ) throws ClassNotFoundException {
+        Class.forName(driver);//Guarantees that the OJP driver is registered with the DriverManager.
+
+        DataSource ds = new DataSource() {
+            @Override
+            public Connection getConnection() throws SQLException {
+                return DriverManager.getConnection(url, user, password);
+            }
+
+            @Override
+            public Connection getConnection(String username, String password) throws SQLException {
+                return DriverManager.getConnection(url, username, password);
+            }
+
+            // The following methods can be left as default or throw UnsupportedOperationException
+            @Override
+            public <T> T unwrap(Class<T> iface) { throw new UnsupportedOperationException(); }
+            @Override
+            public boolean isWrapperFor(Class<?> iface) { throw new UnsupportedOperationException(); }
+            @Override
+            public java.io.PrintWriter getLogWriter() { throw new UnsupportedOperationException(); }
+            @Override
+            public void setLogWriter(java.io.PrintWriter out) { throw new UnsupportedOperationException(); }
+            @Override
+            public void setLoginTimeout(int seconds) { throw new UnsupportedOperationException(); }
+            @Override
+            public int getLoginTimeout() { throw new UnsupportedOperationException(); }
+            @Override
+            public java.util.logging.Logger getParentLogger() { throw new UnsupportedOperationException(); }
+        };
+
         return ds;
     }
 }
